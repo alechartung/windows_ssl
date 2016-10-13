ba2a602b62915092100860c750053e9c3377340b
Alec Hartung <alec.hartung@matrixcare.com>
2016-10-13 12:28:50 -0500

    fix

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures windows_ssl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends			 'windows'
